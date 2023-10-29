f8587fa6ddddc1e457329c6f60fe814890e455a5
Manucrack YT <71397795+ManucrackYT@users.noreply.github.com>
2023-10-29 14:34:41 +0100

    First look files

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    content: ["./themes/default/*.ejs", "./themes/default/**/*.ejs"],
+    theme: {
+        extend: {},
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.gray,
+            red: colors.red,
+            emerald: colors.emerald,
+            indigo: colors.indigo,
+            yellow: colors.yellow,
+            rose: {
+                400: '#be3e76',
+                500: '#d2417c',
+                600: '#d2417c',
+                700: '#be3e76',
+            },
+            dark: {
+                500: '#1e293b',
+                600: '#1e293b',
+                700: '#0f172a',
+                800: '#020617',
+            },
+          },
+    },
+    plugins: [
+        require("@tailwindcss/forms"),
+    ],
+}
