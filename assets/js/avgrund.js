efe4f1f7aafff989a5a0170b2c9122895d90c92f
Manucrack YT <71397795+ManucrackYT@users.noreply.github.com>
2023-11-07 19:08:59 +0100

    1.0

--- assets/js/avgrund.js
+++ assets/js/avgrund.js
@@ -9,8 +9,8 @@
       onBlurContainer: '.container-scroller',
       template: '<p>So implement your design and place content here! If you want to close modal, please hit "Esc", click somewhere on the screen or use special button.</p>' +
         '<div>' +
-        '<a href="http://twitter.com/voronianski" target="_blank" class="twitter btn btn-twitter btn-block">Twitter</a>' +
-        '<a href="http://dribbble.com/voronianski" target="_blank" class="dribble btn btn-dribbble btn-block">Dribbble</a>' +
+        '<a href="" target="_blank" class="twitter btn btn-twitter btn-block">Twitter</a>' +
+        '<a href="" target="_blank" class="dribble btn btn-dribbble btn-block">Dribbble</a>' +
         '</div>' +
         '<div class="text-center mt-4">' +
         '<a href="#" target="_blank" class="btn btn-success mr-2">Great!</a>' +
