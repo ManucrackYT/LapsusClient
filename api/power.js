dfad20084a01e9ec3d307d5e50c465046b71e85b
crazymath072 <102372274+CR072@users.noreply.github.com>
2023-12-23 18:43:13 +0530

    Add restart endpoint

--- api/power.js
+++ api/power.js
@@ -38,6 +38,42 @@ module.exports.load = async function(app, db) {
             return res.json({ "success": false, "message": "Internal server error" });
         }
     });
+        app.get("/api/servers/restart", async (req, res) => {
+        if (!req.session.pterodactyl) return res.redirect("/login");
+        const id = req.query.id;
+        const user = req.query.user;
+    
+        if (!id || !user) {
+            return;
+        }
+    
+        if (user != req.session.userinfo.id) {
+            return res.json({ "success": false, "message": "Unauthorized request" });
+        }
+    
+        try {
+            const response = await fetch(`${settings.pterodactyl.domain}/api/client/servers/${id}/power`, {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${settings.pterodactyl.account_key}`
+                },
+                body: JSON.stringify({ signal: "restart" })
+            });
+    
+            if (!response.ok) {
+                console.error(`Failed to start server. Status: ${response.status}`);
+                return res.json({ "success": false, "message": "Failed to start server" });
+            }
+    
+            console.log("Server started successfully");
+            return res.json({ "success": true, "message": "Operation success!" });
+        } catch (error) {
+            console.error("Error during server start request:", error);
+            return res.json({ "success": false, "message": "Internal server error" });
+        }
+    });
     app.get("/api/servers/stop", async (req, res) => {
         if (!req.session.pterodactyl) return res.redirect("/login");
         const id = req.query.id;
