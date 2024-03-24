6aa25b020fdf05e17f3d208fd5d23fc737bd165a
crazymath072 <102372274+CR072@users.noreply.github.com>
2024-03-24 17:16:01 +0530

    Update power.js

--- api/power.js
+++ api/power.js
@@ -1,20 +1,21 @@
 const settings = require("../settings.json");
 const fetch = require('node-fetch');
 
-module.exports.load = async function(app, db) {
+module.exports.load = async function (app, db) {
     app.get("/api/servers/start", async (req, res) => {
         if (!req.session.pterodactyl) return res.redirect("/login");
         const id = req.query.id;
         const user = req.query.user;
-    
+
         if (!id || !user) {
             return;
         }
-    
+        let check = req.session.pterodactyl.relationships.servers.data.filter(a => a.attributes.identifier == id) || []
+        if (check.length == 0) return res.json({ "success": false, "message": "Unauthorized request" });
         if (user != req.session.userinfo.id) {
             return res.json({ "success": false, "message": "Unauthorized request" });
         }
-    
+
         try {
             const response = await fetch(`${settings.pterodactyl.domain}/api/client/servers/${id}/power`, {
                 method: "POST",
@@ -25,12 +26,12 @@ module.exports.load = async function(app, db) {
                 },
                 body: JSON.stringify({ signal: "start" })
             });
-    
+
             if (!response.ok) {
                 console.error(`Failed to start server. Status: ${response.status}`);
                 return res.json({ "success": false, "message": "Failed to start server" });
             }
-    
+
             return res.json({ "success": true, "message": "Operation success!" });
         } catch (error) {
             console.error("Error during server start request:", error);
@@ -45,15 +46,16 @@ module.exports.load = async function(app, db) {
         if (!req.session.pterodactyl) return res.redirect("/login");
         const id = req.query.id;
         const user = req.query.user;
-    
+
         if (!id || !user) {
             return;
         }
-    
+        let check = req.session.pterodactyl.relationships.servers.data.filter(a => a.attributes.identifier == id) || []
+        if (check.length == 0) return res.json({ "success": false, "message": "Unauthorized request" });
         if (user != req.session.userinfo.id) {
             return res.json({ "success": false, "message": "Unauthorized request" });
         }
-    
+
         try {
             const response = await fetch(`${settings.pterodactyl.domain}/api/client/servers/${id}/power`, {
                 method: "POST",
@@ -64,12 +66,12 @@ module.exports.load = async function(app, db) {
                 },
                 body: JSON.stringify({ signal: "restart" })
             });
-    
+
             if (!response.ok) {
                 console.error(`Failed to restart server. Status: ${response.status}`);
                 return res.json({ "success": false, "message": "Failed to start server" });
             }
-    
+
             return res.json({ "success": true, "message": "Operation success!" });
         } catch (error) {
             console.error("Error during server restart request:", error);
@@ -84,15 +86,16 @@ module.exports.load = async function(app, db) {
         if (!req.session.pterodactyl) return res.redirect("/login");
         const id = req.query.id;
         const user = req.query.user;
-    
+
         if (!id || !user) {
             return;
         }
-    
+        let check = req.session.pterodactyl.relationships.servers.data.filter(a => a.attributes.identifier == id) || []
+        if (check.length == 0) return res.json({ "success": false, "message": "Unauthorized request" });
         if (user != req.session.userinfo.id) {
             return res.json({ "success": false, "message": "Unauthorized request" });
         }
-    
+
         try {
             const response = await fetch(`${settings.pterodactyl.domain}/api/client/servers/${id}/power`, {
                 method: "POST",
@@ -103,12 +106,12 @@ module.exports.load = async function(app, db) {
                 },
                 body: JSON.stringify({ signal: "stop" })
             });
-    
+
             if (!response.ok) {
                 console.error(`Failed to start server. Status: ${response.status}`);
                 return res.json({ "success": false, "message": "Failed to start server" });
             }
-    
+
             console.log("Server stopped successfully");
             return res.json({ "success": true, "message": "Operation success!" });
         } catch (error) {
